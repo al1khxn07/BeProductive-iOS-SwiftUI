4f957eb8694dc59eb8131c133de5ba9635123bd3
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-11-04 20:09:30 -0600

    Initial commit
    Using MVVM.
    Created a scratch of future Todo List.

---
+++ BeProductive/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  BeProductive
+//
+//  Created by Alikhan Zhanibek on 2025-11-04.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
