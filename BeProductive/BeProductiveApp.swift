1111f93bc42dce5a15fc8db8b175e3fa7e364db7
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-11-04 19:32:21 -0600

    Initial Commit

---
+++ BeProductive/BeProductiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeProductiveApp.swift
+//  BeProductive
+//
+//  Created by Alikhan Zhanibek on 2025-11-04.
+//
+
+import SwiftUI
+
+@main
+struct BeProductiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
