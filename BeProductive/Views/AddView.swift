3adebbe3f9bf208f5b66c40beb262e7d0a496bcd
Alikhan Zhanibek <bayalikhan@bk.ru>
2025-11-04 20:11:56 -0600

    initial commitment failure

--- BeProductive/Views/AddView.swift
+++ BeProductive/Views/AddView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    var colorLiteral = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(colorLiteral))
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save".uppercased())
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖊️")
     }
 }
 
 #Preview {
-    AddView()
+    NavigationStack {
+        AddView()
+    }
 }
